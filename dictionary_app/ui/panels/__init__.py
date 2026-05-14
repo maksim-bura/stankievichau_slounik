5d4eb8e3b5f16a10ec5f1aeffa22e6cfe30997b7
maksim-bura <maksim.bura.translator@gmail.com>
2026-05-14 14:30:03 +0300

    Delete dictionary_app directory

--- dictionary_app/ui/panels/__init__.py
+++
@@ -1,4 +0,0 @@
-from .navigation_bar import NavigationBarPanel
-from .sources_toggle import SourcesToggle
-from .splitter_setup import SplitterSetup
-from .entry_list import EntryList
--- dictionary_app/utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from .link_handler import LinkHandler
-from .xml_formatter import format_entry, set_target_subheadword, set_target_senses, clear_target
